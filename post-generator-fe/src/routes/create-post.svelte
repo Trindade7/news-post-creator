a15913098c62389e81ed6d475b4f87ddfcc12738
Trindade7 <trindade.jose77@gmail.com>
2021-04-22 19:27:45 +0100

    added custom scale fn

--- post-generator-fe/src/routes/create-post.svelte
+++ post-generator-fe/src/routes/create-post.svelte
@@ -1,15 +1,29 @@
 <script type="ts">
+	import { onMount } from 'svelte';
+
 	import NewsList from '../components/NewsList.svelte';
 	import NewsBase from '../components/templates/NewsBase.svelte';
+
+	let previewScale = 0.3;
+
+	function resizePreview() {
+		const scale = (window?.innerWidth * 40) / 100000;
+		return Math.max(0.25, Math.min(0.5, scale));
+	}
+
+	onMount(() => {
+		previewScale = resizePreview();
+		window.addEventListener('resize', () => (previewScale = resizePreview()));
+	});
 </script>
 
-<div class="create-post__container  w-full flex flex-col sm:flex-row justify-center">
-	<div class="content__preview relative">
-		<div class="post-preview">
+<div class="create-post__container  w-full flex flex-col sm:flex-row justify-center px-4 sm:px-6">
+	<div class="content__preview relative overflow-hidden">
+		<div class="post-preview w-full" style="transform: scale({previewScale})">
 			<NewsBase />
 		</div>
 	</div>
-	<div class="content__news max-w-md px-4 py-5 sm:px-6">
+	<div class="content__news max-w-md pl-4 py-5">
 		<NewsList />
 	</div>
 </div>
@@ -17,6 +31,5 @@
 <style>
 	.post-preview {
 		transform-origin: 0 0;
-		transform: scale(0.25);
 	}
 </style>
